4ece8ffb5c63f8e8577ae06b7efa8c460dd8f9d1
icecarti <and08sha@gmail.com>
2022-10-05 14:32:58 +0300

    done

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,8 +1,10 @@
 import typing as tp
 
+
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -31,6 +33,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
