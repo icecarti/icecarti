aee16dc314869aa7f764eb40c0023e22643b9b49
icecarti <83011564+icecarti@users.noreply.github.com>
2022-11-16 02:30:47 +0300

    Update porcelain.py

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -1,6 +1,5 @@
 import os
 import pathlib
-import shutil
 import typing as tp
 
 from pyvcs.index import read_index, update_index
@@ -10,41 +9,15 @@ from pyvcs.tree import commit_tree, write_tree
 
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
-    update_index(gitdir, paths)
+    # PUT YOUR CODE HERE
+    ...
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    index = read_index(gitdir)
-    res = commit_tree(gitdir, write_tree(gitdir, index), message, author)
-    return res
+    # PUT YOUR CODE HERE
+    ...
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    head = gitdir / "refs" / "heads" / obj_name
-
-    if head.exists():
-        with head.open(mode="r") as f:
-            obj_name = f.read()
-
-    index = read_index(gitdir)
-
-    for file in index:
-        if pathlib.Path(file.name).is_file():
-            if "/" in file.name:
-                shutil.rmtree(file.name[: file.name.index("/")])
-            else:
-                os.remove(file.name)
-
-    obj_path = gitdir / "objects" / obj_name[:2] / obj_name[2:]
-
-    with obj_path.open(mode="rb") as f1:
-        sha = commit_parse(f1.read()).decode()
-
-    for file1 in find_tree_files(sha, gitdir):
-        if "/" in file1[0]:
-            dir_name = file1[0][: file1[0].find("/")]
-            pathlib.Path(dir_name).absolute().mkdir()
-
-        with open(file1[0], "w") as f2:
-            header, content = read_object(file1[1], gitdir)
-            f2.write(content.decode())
+    # PUT YOUR CODE HERE
+    ...
