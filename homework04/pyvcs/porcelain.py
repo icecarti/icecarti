a69f048c4f1391fa7a9e2297b589ccf48df1ce53
icecarti <and08sha@gmail.com>
2022-11-16 02:27:08 +0300

    first

--- homework04/pyvcs/porcelain.py
+++ homework04/pyvcs/porcelain.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import shutil
 import typing as tp
 
 from pyvcs.index import read_index, update_index
@@ -9,15 +10,41 @@ from pyvcs.tree import commit_tree, write_tree
 
 
 def add(gitdir: pathlib.Path, paths: tp.List[pathlib.Path]) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    update_index(gitdir, paths)
 
 
 def commit(gitdir: pathlib.Path, message: str, author: tp.Optional[str] = None) -> str:
-    # PUT YOUR CODE HERE
-    ...
+    index = read_index(gitdir)
+    res = commit_tree(gitdir, write_tree(gitdir, index), message, author)
+    return res
 
 
 def checkout(gitdir: pathlib.Path, obj_name: str) -> None:
-    # PUT YOUR CODE HERE
-    ...
+    head = gitdir / "refs" / "heads" / obj_name
+
+    if head.exists():
+        with head.open(mode="r") as f:
+            obj_name = f.read()
+
+    index = read_index(gitdir)
+
+    for file in index:
+        if pathlib.Path(file.name).is_file():
+            if "/" in file.name:
+                shutil.rmtree(file.name[: file.name.index("/")])
+            else:
+                os.remove(file.name)
+
+    obj_path = gitdir / "objects" / obj_name[:2] / obj_name[2:]
+
+    with obj_path.open(mode="rb") as f1:
+        sha = commit_parse(f1.read()).decode()
+
+    for file1 in find_tree_files(sha, gitdir):
+        if "/" in file1[0]:
+            dir_name = file1[0][: file1[0].find("/")]
+            pathlib.Path(dir_name).absolute().mkdir()
+
+        with open(file1[0], "w") as f2:
+            header, content = read_object(file1[1], gitdir)
+            f2.write(content.decode())
--- homework04/pyvcs/repo.py
+++ homework04/pyvcs/repo.py
@@ -4,10 +4,33 @@ import typing as tp
 
 
 def repo_find(workdir: tp.Union[str, pathlib.Path] = ".") -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    root = os.getenv("GIT_DIR", ".git")
+
+    workdir = pathlib.Path(str(pathlib.Path(workdir) / root).split(root)[0]) / root
+    if not os.path.exists(workdir):
+        raise Exception("Not a git repository")
+    return pathlib.Path(workdir)
 
 
 def repo_create(workdir: tp.Union[str, pathlib.Path]) -> pathlib.Path:
-    # PUT YOUR CODE HERE
-    ...
+    root = os.getenv("GIT_DIR", ".git")
+    workdir = pathlib.Path(workdir)
+    if workdir.is_file():
+        raise Exception(f"{workdir.name} is not a directory")
+
+    os.makedirs(workdir / root / "refs" / "heads")
+    os.makedirs(workdir / root / "refs" / "tags")
+    os.makedirs(workdir / root / "objects")
+
+    with open(workdir / root / "HEAD", "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(workdir / root / "config", "w") as f:
+        f.write(
+            "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n"
+        )
+
+    with open(workdir / root / "description", "w") as f:
+        f.write("Unnamed pyvcs repository.\n")
+
+    return pathlib.Path(workdir / root)
