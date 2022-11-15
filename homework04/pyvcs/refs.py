156eccd8053923f6f3ddb834ab9038f2d94175fb
icecarti <83011564+icecarti@users.noreply.github.com>
2022-11-16 02:31:25 +0300

    Update refs.py

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -3,34 +3,30 @@ import typing as tp
 
 
 def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value: str) -> None:
-    path = gitdir / ref
-    f = path.open("w")
-    f.write(new_value)
-    f.close()
+    # PUT YOUR CODE HERE
+    ...
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
-    (gitdir / name).open("w").write(ref)
+    # PUT YOUR CODE HERE
+    ...
 
 
 def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
-    refname = get_ref(gitdir) if refname == "HEAD" else refname
-    if is_detached(gitdir):
-        return refname
-    return (gitdir / pathlib.Path(refname)).open("r").read()
+    # PUT YOUR CODE HERE
+    ...
 
 
 def resolve_head(gitdir: pathlib.Path) -> tp.Optional[str]:
-    return ref_resolve(gitdir, "HEAD") if (gitdir / get_ref(gitdir)).exists() else None
+    # PUT YOUR CODE HERE
+    ...
 
 
 def is_detached(gitdir: pathlib.Path) -> bool:
-    return "ref" not in (gitdir / "HEAD").open("r").read()
+    # PUT YOUR CODE HERE
+    ...
 
 
 def get_ref(gitdir: pathlib.Path) -> str:
-    path = gitdir / "HEAD"
-    detached = is_detached(gitdir)
-    with path.open("r") as f:
-        ref = f.read()[5:-1] if not detached else f.read()
-    return ref
+    # PUT YOUR CODE HERE
+    ...
