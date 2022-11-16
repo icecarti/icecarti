c2317c96c7047d015449568846db817f7362a2e1
icecarti <83011564+icecarti@users.noreply.github.com>
2022-11-16 03:14:01 +0300

    Update test_repo.py

--- homework04/tests/test_repo.py
+++ homework04/tests/test_repo.py
@@ -1,86 +1,69 @@
-import os
 import pathlib
+import stat
+import time
+import unittest
+from unittest.mock import patch
 
+import pyvcs
 from pyfakefs.fake_filesystem_unittest import TestCase
-from pyvcs import repo
+from pyvcs.index import read_index, update_index
+from pyvcs.repo import repo_create
+from pyvcs.tree import commit_tree, write_tree
 
 
-class RepoCreateTestCase(TestCase):
+@unittest.skipIf(pyvcs.__version_info__ < (0, 5, 0), "Нужна версия пакета 0.5.0 и выше")
+class WriteTreeTestCase(TestCase):
     def setUp(self):
-        os.environ["GIT_DIR"] = ".git"
         self.setUpPyfakefs()
 
-    def test_created_repo_has_the_correct_structure(self):
-        workdir = pathlib.Path(".")
-        gitdir = repo.repo_create(workdir)
+    def test_write_tree(self):
+        gitdir = repo_create(".")
+        animals = pathlib.Path("animals.txt")
+        mode100644 = stat.S_IFREG | stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
+        self.fs.create_file(
+            animals,
+            contents="Big blue basilisks bawl in the basement\n",
+            st_mode=mode100644,
+        )
+        update_index(gitdir, [animals], write=True)
+        entries = read_index(gitdir)
+        sha = write_tree(gitdir, entries)
+        self.assertEqual("dc6b8ea09fb7573a335c5fb953b49b85bb6ca985", sha)
 
-        expected_gitdir = workdir / ".git"
-        self.assertEqual(expected_gitdir, gitdir)
-        self.assertTrue(gitdir.exists())
-        self.assertTrue((gitdir / "refs" / "heads").exists())
-        self.assertTrue((gitdir / "refs" / "tags").exists())
-        self.assertTrue((gitdir / "objects").exists())
+    def test_write_tree_subdirs(self):
+        gitdir = repo_create(".")
+        mode100644 = stat.S_IFREG | stat.S_IRUSR | stat.S_IWUSR | stat.S_IRGRP | stat.S_IROTH
+        quote = pathlib.Path("quote.txt")
+        self.fs.create_file(quote, contents="that's what she said", st_mode=mode100644)
+        letters = pathlib.Path("alphabeta") / "letters.txt"
+        self.fs.create_file(letters, contents="abcdefg", st_mode=mode100644)
+        digits = pathlib.Path("numbers") / "digits.txt"
+        self.fs.create_file(digits, contents="1234567890", st_mode=mode100644)
+        update_index(gitdir, [quote, letters, digits], write=True)
+        entries = read_index(gitdir)
+        sha = write_tree(gitdir, entries)
+        self.assertEqual("a9cde03408c68cbb205b038140b4c3a38aa1d01a", sha)
 
-        head = gitdir / "HEAD"
-        self.assertTrue(head.exists())
-        with head.open() as f:
-            self.assertEqual("ref: refs/heads/master\n", f.read())
+        alphabeta_tree_sha = "7926bf494dcdb82261e1ca113116610f8d05470b"
+        alphabeta_tree_obj = gitdir / "objects" / alphabeta_tree_sha[:2] / alphabeta_tree_sha[2:]
+        self.assertTrue(alphabeta_tree_obj.exists())
 
-        config = gitdir / "config"
-        self.assertTrue(config.exists())
-        with config.open() as f:
-            self.assertEqual(
-                "[core]\n\trepositoryformatversion = 0\n\tfilemode = true\n\tbare = false\n\tlogallrefupdates = false\n",
-                f.read(),
-            )
-
-        description = gitdir / "description"
-        self.assertTrue(description.exists())
-        with description.open() as f:
-            self.assertEqual(
-                "Unnamed pyvcs repository.\n", f.read(),
-            )
-
-    def test_cant_create_repo_if_workdir_is_a_file(self):
-        filename = "test"
-        workdir = pathlib.Path(filename)
-        self.fs.create_file(workdir, contents="test")
-        with self.assertRaises(Exception) as ctx:
-            repo.repo_create(workdir)
-        self.assertEqual(f"{filename} is not a directory", str(ctx.exception))
-
-    def test_git_dir(self):
-        dir_name = ".pyvcs"
-        os.environ["GIT_DIR"] = dir_name
-        workdir = pathlib.Path(".")
-        actual_gitdir = repo.repo_create(workdir)
-        expected_gitdir = workdir / dir_name
-        self.assertEqual(expected_gitdir, actual_gitdir)
-        self.assertTrue(expected_gitdir.exists())
+        numbers_tree_sha = "32ad3641a773ce34816dece1ce63cc24c8a514d0"
+        numbers_tree_obj = gitdir / "objects" / numbers_tree_sha[:2] / numbers_tree_sha[2:]
+        self.assertTrue(numbers_tree_obj.exists())
 
 
-class RepoFindTestCase(TestCase):
+@unittest.skipIf(pyvcs.__version_info__ < (0, 6, 0), "Нужна версия пакета 0.6.0 и выше")
+class CommitTreeTestCase(TestCase):
     def setUp(self):
-        os.environ["GIT_DIR"] = ".git"
         self.setUpPyfakefs()
 
-    def test_repo_find(self):
-        workdir = pathlib.Path(".")
-        workdir = workdir.absolute()
-        expected_gitdir = workdir / ".git"
-        self.fs.create_dir(expected_gitdir)
-        gitdir = repo.repo_find(workdir)
-        self.assertEqual(expected_gitdir, gitdir)
-
-    def test_repo_find_in_a_given_dir(self):
-        expected_gitdir = pathlib.Path("dir1") / ".git"
-        expected_gitdir = expected_gitdir.absolute()
-        current_dir = expected_gitdir / "dir2"
-        self.fs.create_dir(current_dir)
-        gitdir = repo.repo_find(current_dir)
-        self.assertEqual(expected_gitdir, gitdir)
-
-    def test_repo_not_found(self):
-        with self.assertRaises(Exception) as ctx:
-            _ = repo.repo_find()
-        self.assertEqual("Not a git repository", str(ctx.exception))
+    @patch("time.timezone", -10800)
+    @patch("time.localtime", return_value=time.localtime(1593638381))
+    def test_commit_tree(self, localtime):
+        gitdir = repo_create(".")
+        tree_sha = "dc6b8ea09fb7573a335c5fb953b49b85bb6ca985"
+        author = "Dmitriy Sorokin <Dementiy@yandex.ru>"
+        message = "initial commit"
+        commit_sha = commit_tree(gitdir, tree_sha, message, parent=None, author=author)
+        self.assertEqual("f60952d53906d8b2e6f8567762f86fbe7ca4ac65", commit_sha)
